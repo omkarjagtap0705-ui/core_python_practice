07c9e903eebcee09f343c1a8ddf41a3db9c61c1d
omkarjagtap0705-ui <omkarjagtap0705@gmail.com>
2026-08-04 22:42:06 +0530

    Add files via upload

---
+++ aug4_task.py
@@ -0,0 +1,48 @@
+                                        # Task_1
+
+name = "Omkar Jagtap"
+age = 21
+city = "Pune"
+dob = "11/7/2005"
+college = "Kaveri College of Arts, Science & Commerce"
+blood_g = "AB+"
+
+print("\n")
+print("The output of the task 1 is:")
+print("----------- MY IDENTITY CARD-----------")
+print("\n")
+print(f'Name : {name}')
+print(f'Age : {age}')
+print(f'City : {city}')
+print(f'DOB : {dob}')
+print(f'College : {college}')
+print(f'Blood Group : {blood_g}')
+print("\n")
+
+                                        # Task_2
+
+a = 25
+b = 25.5
+c = "Twenty Five"
+d = True
+e = 2+3j
+
+print("\n")
+print("The output of the task 2 is:")
+print(f'The data type of the variable {a} is {type(a)}')
+print(f'The data type of the variable {b} is {type(b)}')
+print(f'The data type of the variable {c} is {type(c)}')
+print(f'The data type of the variable {d} is {type(d)}')
+print(f'The data type of the variable {e} is {type(e)}')
+print("\n")
+
+                                    # Task_3
+v1 = 100
+v2 = 200
+v3 = 100
+
+print("\n")
+print("The output of the task 3 is:")
+print(f'The memory location of the variable {v1} is {id(v1)}')
+print(f'The memory location of the variable {v2} is {id(v2)}')
+print(f'The memory location of the variable {v1} is {id(a)}')
